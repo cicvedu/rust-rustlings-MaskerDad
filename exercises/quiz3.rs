0537666f56ce370832be4a028bb30e54ea0c5573
zq <zhouquan1511@163.com>
2023-08-17 21:02:40 +0800

    8.17

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -28,6 +27,10 @@ impl ReportCard {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+    pub fn print_g(&self) -> String{
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, "A+")
     }
 }
 
@@ -57,7 +60,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print_g(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
